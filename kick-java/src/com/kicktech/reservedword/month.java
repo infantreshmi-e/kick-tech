ffbcea17b6b5be16d5043060f4382938bb6848b6
infantreshmi-e <infantreshmi1995@gmail.com>
2021-10-20 21:15:50 +0530

    Reserved word commit

---
+++ kick-java/src/com/kicktech/reservedword/month.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.kicktech.reservedword;
+
+/**
+ * @author INFANT RESHMI E
+ *
+ */
+public enum month {
+ January,February,March,April,May,June,July,August,September,October,November,December
+}
